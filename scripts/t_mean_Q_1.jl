1897537d3dbd21ca77db756ff6ae1f8a77e44ab5
SheikhParvezMandal <sheikh.parvezmandal@gmail.com>
2024-04-04 14:40:25 +0200

    3

--- scripts/t_mean_Q_1.jl
+++ scripts/t_mean_Q_1.jl
@@ -19,15 +19,15 @@ ITensors.op(::OpName"ρ", ::SiteType"S=1/2") = ρ
 
 ## time evolution parameters
 cutoff = 1E-8
-maxdim = 50
+maxdim = 1000
 tau = 10^-2             ## time step duration
-nt = 350
+nt = 300
 ttotal = nt*tau             ## TOTAL TIME evolution
 
-N_chain = 120            ## Number of chain sites for single chain-transformed environment
+N_chain = 90            ## Number of chain sites for single chain-transformed environment
 tot_chain = 2*N_chain+1
 
-boson_dim = 5
+boson_dim = 8
 
 # Make an array of 'site' INDICES for the (spin+chain)
 S_pos = N_chain+1
@@ -45,11 +45,11 @@ ITensors.op(::OpName"0", ::SiteType"Qudit", d::Int) = zer0
 ω_0 = 1                 ## spin splitting
 Ω = 0                   ## independent model if Ω = 0
 
-T = 5;                  ## temperature of bath
+T = .1;                  ## temperature of bath
 β = 1/T 
 ###
 
-u = 0.01 # counting field parameter
+u = 0.001 # counting field parameter
 
 
 ## 
@@ -57,49 +57,79 @@ u = 0.01 # counting field parameter
 p=plot()
 
 t_list = collect(0:1:nt)*tau
-α_list = [0.1,1]
+α_list = [0.2,0.4]
 #α = 1
 
 #cat(a, b) = reshape(append!(vec(a), vec(b)), size(a)[1:end-1]..., :)
+support_cutoff = 6*10^2
+supp = (0, support_cutoff)                 ## support of the weight function
+Nquad = 10^8          ## Number of quadrature points
+
+n(k) = 1/(exp(β*k) - 1)
 
 for α = α_list
 
-#J(t) = 2*α*t*exp(-t/ω_C)
+N_coeff = N_chain + 1
+w_fn1(k) = (2*α*k*exp(-k/ω_C))*(1 + n(k))           ## weight function for real space bath
+w_fn2(k) = (2*α*k*exp(-k/ω_C))*n(k)           ## weight function for tilde space bath
+η01 = quadgk(w_fn1, 0, support_cutoff)
+c_01 = sqrt(η01[1])
+η02 = quadgk(w_fn2, 0, support_cutoff)
+c_02 = sqrt(η02[1])
+ab1 = recur_coeff(w_fn1, supp, N_coeff, Nquad)    ## recurrence coefficients for for real space bath
+ab2 = recur_coeff(w_fn2, supp, N_coeff, Nquad)    ## recurrence coefficients for for tilde space bath
 
+#chi_pu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, u, cutoff, maxdim)
 
+count_dates = exp_xHB(ab1, im*u, s_total);
+evol = unit_gates(ω_0, Ω, c_01, c_02, ab1, ab2, tau, s_total)
 
-    #c_0 = ω_C * sqrt(2 * α / pi)
+################################################################################################
+ρ = init_state(s_total)  ## initial state of (spin+chain);
+################################################################################################
+
+chi_pu=Vector{ComplexF64}()
+
+U_ρ_Ud = ρ
+cd_U_ρ_Ud = apply(count_dates, U_ρ_Ud; cutoff, maxdim)
+
+chi = tr(cd_U_ρ_Ud)
     
-    N_coeff = N_chain + 1
-    Nquad = 10^7          ## Number of quadrature points
-    n(k) = 1/(exp(β*k) - 1)
-    w_fn1(k) = (2*α*k*exp(-k/ω_C))*(1 + n(k))           ## weight function for real space bath
-    w_fn2(k) = (2*α*k*exp(-k/ω_C))*n(k)           ## weight function for tilde space bath
-    η01 = quadgk(w_fn1, 0, 10^3)
-    c_01 = sqrt(η01[1])
-    η02 = quadgk(w_fn2, 0, 10^3)
-    c_02 = sqrt(η02[1])
-    supp = (0, 10^3)                 ## support of the weight function
-    ab1 = recur_coeff(w_fn1, supp, N_coeff, Nquad)    ## recurrence coefficients for for real space bath
-    ab2 = recur_coeff(w_fn2, supp, N_coeff, Nquad)    ## recurrence coefficients for for tilde space bath
+push!(chi_pu,chi[1])
 
-    chi_pu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, u, cutoff, maxdim)
+
+for t = 1:1:nt
+
+    U_ρ_Ud = apply(evol, U_ρ_Ud; cutoff, maxdim, apply_dag=true)
+    cd_U_ρ_Ud = apply(count_dates, U_ρ_Ud; cutoff, maxdim)
+
+    chi = tr(cd_U_ρ_Ud)[1]
+
+    @show imag(chi)/u
+
     
-    mean_Q = imag.(chi_pu)/u
+    push!(chi_pu,chi)
 
-    plot!(t_list,real(mean_Q),label= "α = $α")
-    @show last(real(mean_Q))
+end
+
+
+mean_Q = imag.(chi_pu)/u
+@show real(mean_Q)
+
+#plot!(t_list,real(mean_Q),label= "α = $α")
+
+plot!(collect(0:1:length(mean_Q)-1)*tau,real(mean_Q),label= "α = $α")
 
 end
 
 
 plot!(legend=:topright)
 xlabel!("t")
-title = string("N_chain = ", N_chain,", boson_dim = ", boson_dim,", u = ",u)
+title = string("<Q>, N_ch = ", N_chain,", b_dim = ", boson_dim,", u = ",u, ", k_max = ", support_cutoff)
 title!(title)
 display("image/png", p)
 
-#= 
-a = "plots\\meanq_t_a_2.png";
+
+a = "meanq_1.png";
 savefig(a)
-     =#
+    
--- scripts/t_var_Q_1.jl
+++ scripts/t_var_Q_1.jl
@@ -19,15 +19,15 @@ ITensors.op(::OpName"ρ", ::SiteType"S=1/2") = ρ
 
 ## time evolution parameters
 cutoff = 1E-8
-maxdim = 30
+maxdim = 900
 tau = 10^-2             ## time step duration
-nt = 200
+nt = 150
 ttotal = nt*tau             ## TOTAL TIME evolution
 
-N_chain = 2            ## Number of chain sites for single chain-transformed environment
+N_chain = 90            ## Number of chain sites for single chain-transformed environment
 tot_chain = 2*N_chain+1
 
-boson_dim = 3
+boson_dim = 8
 
 # Make an array of 'site' INDICES for the (spin+chain)
 S_pos = N_chain+1
@@ -45,11 +45,10 @@ ITensors.op(::OpName"0", ::SiteType"Qudit", d::Int) = zer0
 ω_0 = 1                 ## spin splitting
 Ω = 0                   ## independent model if Ω = 0
 
-T = 5;                  ## temperature of bath
-β = 1/T 
+
 ###
 
-u = 0.01 # counting field parameter
+u = 0.001 # counting field parameter
 
 
 ## 
@@ -57,54 +56,52 @@ u = 0.01 # counting field parameter
 p=plot()
 
 t_list = collect(0:1:nt)*tau
-#α_list = [0.1,1.5]
-α = 1
+T_list = [0.1,1]
+α = .1
 
 #cat(a, b) = reshape(append!(vec(a), vec(b)), size(a)[1:end-1]..., :)
+support_cutoff = 6*10^2
+supp = (0, support_cutoff)                 ## support of the weight function
+Nquad = 10^8          ## Number of quadrature points
+N_coeff = N_chain + 1
 
-#for α = α_list
-
-#J(t) = 2*α*t*exp(-t/ω_C)
 
 
-
-    #c_0 = ω_C * sqrt(2 * α / pi)
-    
-    N_coeff = N_chain + 1
-    Nquad = 5000 * N_coeff            ## Number of quadrature points
+for T = T_list
+    β = 1/T 
     n(k) = 1/(exp(β*k) - 1)
     w_fn1(k) = (2*α*k*exp(-k/ω_C))*(1 + n(k))           ## weight function for real space bath
     w_fn2(k) = (2*α*k*exp(-k/ω_C))*n(k)           ## weight function for tilde space bath
-    η01 = quadgk(w_fn1, 0, +Inf)
+    η01 = quadgk(w_fn1, 0, support_cutoff)
     c_01 = sqrt(η01[1])
-    η02 = quadgk(w_fn2, 0, +Inf)
+    η02 = quadgk(w_fn2, 0, support_cutoff)
     c_02 = sqrt(η02[1])
-    supp = (0, 10000)                 ## support of the weight function
     ab1 = recur_coeff(w_fn1, supp, N_coeff, Nquad)    ## recurrence coefficients for for real space bath
     ab2 = recur_coeff(w_fn2, supp, N_coeff, Nquad)    ## recurrence coefficients for for tilde space bath
+
 
     chi_pu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, u, cutoff, maxdim)
     chi_mu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, -u, cutoff, maxdim)
     chi_0 = ones(length(chi_mu),1)
 
-    mean_Q = (-im)*(log(chi_pu)-log(chi_mu))/(2*u)
+    #mean_Q = (-im)*(log(chi_pu)-log(chi_mu))/(2*u)
 
-    var_Q = (-1)*(log(chi_pu)+log(chi_mu))/(u^2)
-    mean_Q = imag.(chi_pu)/u
+    var_Q = (-1)*(log.(chi_pu)+log.(chi_mu))/(u^2)
+    #mean_Q = imag.(chi_pu)/u
 
-    plot!(t_list,real(mean_Q),label= "α = $α")
-    @show last(real(mean_Q))
+    plot!(t_list,real(var_Q),label= "T = $T, α = 0.1")
+    @show var_Q
 
-#end
+end
+
 
 
 plot!(legend=:topright)
 xlabel!("t")
-title = string("N_chain = ", N_chain,", boson_dim = ", boson_dim,", u = ",u)
+title = string("<<Q>>, N_ch = ", N_chain,", b_dim = ", boson_dim,", u = ",u, ", k_max = ", support_cutoff)
 title!(title)
 display("image/png", p)
 
-#= 
-a = "plots\\meanq_t_a_2.png";
+
+a = "varq_1.png";
 savefig(a)
-     =#
