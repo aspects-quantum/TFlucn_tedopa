40ebdbc6faa45fb0f5b92ad07f40bf03b2c15866
SheikhParvezMandal <121781462+SheikhParvezMandal@users.noreply.github.com>
2024-06-04 16:08:50 +0200

    5

--- scripts/t_mean_Q_1.jl
+++ scripts/t_mean_Q_1.jl
@@ -11,8 +11,8 @@ ITensors.disable_warn_order()
 
 
 
-ρ = [1 0;0 0]
-ρ = ρ/tr(ρ)
+ρ = [1 0;0 0]    
+ρ = ρ/tr(ρ)             ## initial spin state
 
 ITensors.op(::OpName"ρ", ::SiteType"S=1/2") = ρ
 
@@ -62,8 +62,8 @@ t_list = collect(0:1:nt)*tau
 
 #cat(a, b) = reshape(append!(vec(a), vec(b)), size(a)[1:end-1]..., :)
 support_cutoff = 7*10^2
-supp = (0, support_cutoff)                 ## support of the weight function
-Nquad = 10^7              ## Number of quadrature points
+supp = (0, support_cutoff)                            ## support of the weight function
+Nquad = 10^7                                          ## Number of quadrature points
 N_coeff = N_chain + 1
 
 ab1 = Matrix{Float64}(undef,N_coeff,2)
@@ -73,58 +73,16 @@ n(k) = 1/(exp(β*k) - 1)
 
 #for α = α_list
 
-    w_fn1(k) = (2*α*k*exp(-k/ω_C))*(1 + n(k))           ## weight function for real space bath
-    w_fn2(k) = (2*α*k*exp(-k/ω_C))*n(k)           ## weight function for tilde space bath
+    w_fn1(k) = (2*α*k*exp(-k/ω_C))*(1 + n(k))         ## weight function for real space bath
+    w_fn2(k) = (2*α*k*exp(-k/ω_C))*n(k)               ## weight function for tilde space bath
     η01 = quadgk(w_fn1, 0, support_cutoff)
     c_01 = sqrt(η01[1])
     η02 = quadgk(w_fn2, 0, support_cutoff)
     c_02 = sqrt(η02[1])
-    
-    if N_chain >= 90
-        ab1[1:90,1:2] = recur_coeff(w_fn1, supp, 90, Nquad)    ## recurrence coefficients for for real space bath
-        ab2[1:90,1:2] = recur_coeff(w_fn2, supp, 90, Nquad)    ## recurrence coefficients for for tilde space bath
-        
-        a1_100 = ab1[90,1]; b1_100 = ab1[90,2]; a2_100 = ab2[90,1]; b2_100 = ab2[90,2]
-        [ab1[i,1] = a1_100  for i = 91:N_coeff]; [ab1[i,2] = b1_100  for i = 91:N_coeff]; 
-        [ab2[i,1] = a2_100  for i = 91:N_coeff]; [ab2[i,2] = b2_100  for i = 91:N_coeff]; 
-    else
-        ab1[1:N_coeff,1:2] = recur_coeff(w_fn1, supp, N_coeff, Nquad)    ## recurrence coefficients for for real space bath
-        ab2[1:N_coeff,1:2] = recur_coeff(w_fn2, supp, N_coeff, Nquad)    ## recurrence coefficients for for tilde space bath
-    end
+    ab1 = recur_coeff(w_fn1, supp, N_coeff, Nquad)    ## recurrence coefficients for for real space bath
+    ab2 = recur_coeff(w_fn2, supp, N_coeff, Nquad)    ## recurrence coefficients for for tilde space bath
 
-    #chi_pu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, u, cutoff, maxdim)
-
-    count_dates = exp_xHB(ab1, im*u, s_total);
-    evol = unit_gates(ω_0, Ω, c_01, c_02, ab1, ab2, tau, s_total)
-
-    ################################################################################################
-    ρ = init_state(s_total)  ## initial state of (spin+chain);
-    ################################################################################################
-
-    chi_pu=Vector{ComplexF64}()
-
-    U_ρ_Ud = ρ
-    cd_U_ρ_Ud = apply(count_dates, U_ρ_Ud; cutoff, maxdim)
-
-    chi = tr(cd_U_ρ_Ud)
-        
-    push!(chi_pu,chi[1])
-
-
-    for t = 1:1:nt
-
-        U_ρ_Ud = apply(evol, U_ρ_Ud; cutoff, maxdim, apply_dag=true)
-        cd_U_ρ_Ud = apply(count_dates, U_ρ_Ud; cutoff, maxdim)
-
-        chi = tr(cd_U_ρ_Ud)[1]
-
-        @show imag(chi)/u
-
-        
-        push!(chi_pu,chi)
-
-    end
-
+    chi_pu = charfn(ω_0, Ω, c_01, c_02, ab1, ab2, s_total, tau, nt, u, cutoff, maxdim)
 
     mean_Q = imag.(chi_pu)/u
     @show real(mean_Q)
